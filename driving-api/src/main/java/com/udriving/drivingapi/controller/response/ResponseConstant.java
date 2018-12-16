9750ca3730f331afed522d7ffdb3b087256b1207
liuhao <liuhaoduoduo@163.com>
2018-12-16 18:12:20 +0800

    新增接口返回数据结构定义，新增接口响应状态码定义。

---
+++ driving-api/src/main/java/com/udriving/drivingapi/controller/response/ResponseConstant.java
@@ -0,0 +1,15 @@
+package com.udriving.drivingapi.controller.response;
+
+/**
+ * 接口返回体常量
+ */
+public interface ResponseConstant {
+    /**
+     * 失败
+     */
+    byte ERROR = -1;
+    /**
+     * 成功
+     */
+    byte SUCCEED = 0;
+}
