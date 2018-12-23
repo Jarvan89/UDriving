aa544201a7d13c11278c1cb17d12bf41fc8a0413
liuhao <liuhaoduoduo@163.com>
2018-12-23 13:55:32 +0800

    细化接口调用失败的状态码

--- driving-api/src/main/java/com/udriving/drivingapi/controller/response/ResponseConstant.java
+++ driving-api/src/main/java/com/udriving/drivingapi/controller/response/ResponseConstant.java
@@ -5,7 +5,15 @@ package com.udriving.drivingapi.controller.response;
  */
 public interface ResponseConstant {
     /**
-     * 失败
+     * 失败-活动未找到
+     */
+    byte SAVE_FAIL = -3;
+    /**
+     * 失败-活动未找到
+     */
+    byte ACTIVITY_NOT_FIND = -2;
+    /**
+     * 失败-通用原因未明确的失败均用此值
      */
     byte ERROR = -1;
     /**
