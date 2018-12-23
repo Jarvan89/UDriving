11bc1b0f0823f3b2aa29f8165088dccaf53f8d41
liuhao <liuhaoduoduo@163.com>
2018-12-23 12:41:35 +0800

    根据活动状态查询活动列表

--- driving-api/src/main/java/com/udriving/drivingapi/entity/activity/ActivityRepository.java
+++ driving-api/src/main/java/com/udriving/drivingapi/entity/activity/ActivityRepository.java
@@ -2,8 +2,17 @@ package com.udriving.drivingapi.entity.activity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
- *活动实体类数据库操作接口
+ * 活动实体类数据库操作接口
  */
 public interface ActivityRepository extends JpaRepository<UDActiviti, Integer> {
+    /**
+     * 根据活动状态查询活动列表
+     *
+     * @param status 活动状态
+     * @return 满足条件的活动列表
+     */
+    List<UDActiviti> findByStatus(int status);
 }
