4e35a298b8a6500f6a1febda3dd1d83dcd90fbd8
liuhao <liuhaoduoduo@163.com>
2019-01-01 22:07:07 +0800

    新增：地址信息实体类

---
+++ driving-api/src/main/java/com/udriving/drivingapi/entity/activity/AddressInfo.java
@@ -0,0 +1,34 @@
+package com.udriving.drivingapi.entity.activity;
+
+import lombok.Data;
+
+/**
+ * 地址信息实体
+ */
+@Data
+public class AddressInfo {
+    /**
+     * 经度
+     */
+    private float longitude;
+    /**
+     * 纬度
+     */
+    private float latitude;
+    /**
+     * 省
+     */
+    private String province;
+    /**
+     * 市
+     */
+    private String city;
+    /**
+     * 区县
+     */
+    private String county;
+    /**
+     * 完整地址
+     */
+    private String address;
+}
