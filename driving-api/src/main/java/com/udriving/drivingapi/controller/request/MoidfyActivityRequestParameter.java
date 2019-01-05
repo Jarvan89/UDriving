42e1b09f5ec49c360646826ee1858a80438127dc
liuhao <liuhaoduoduo@163.com>
2019-01-05 21:56:08 +0800

    新增修改活动接口的入参和返回数据结构

---
+++ driving-api/src/main/java/com/udriving/drivingapi/controller/request/MoidfyActivityRequestParameter.java
@@ -0,0 +1,69 @@
+package com.udriving.drivingapi.controller.request;
+
+import com.udriving.drivingapi.entity.activity.AddressInfo;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 修改活动接口请求参数
+ */
+@Data
+public class MoidfyActivityRequestParameter {
+    /**
+     * 活动id
+     */
+    protected Integer id;
+    /**
+     * 活动名
+     */
+    protected String title;
+    /**
+     * 预估费用
+     */
+    protected float estimateCost;
+    /**
+     * 创建人用户名，该名仅针对本次活动有效
+     */
+    private String createUserName;
+    /**
+     * 活动介绍图片
+     */
+    private List<String> introducePicture;
+    /**
+     * 出发地信息
+     */
+    private AddressInfo departAddressInfo;
+    /**
+     * 目的地信息
+     */
+    private AddressInfo destinationAddressInfo;
+    /**
+     * 出发时间
+     */
+    private long departTimestamp;
+    /**
+     * 返回时间
+     */
+    private long backTimestamp;
+    /**
+     * 成员列表
+     */
+    private List<String> memberIdList;
+    /**
+     * 微信群聊二维码
+     */
+    private String weChatFlockQrCode;
+    /**
+     * 参加活动的车辆id
+     */
+    private List<String> carNumber;
+    /**
+     * 路线图片
+     */
+    private String pathImage;
+    /**
+     * 注意事项
+     */
+    private String notes;
+}
---
+++ driving-api/src/main/java/com/udriving/drivingapi/controller/response/MoidfyActivityResponse.java
@@ -0,0 +1,11 @@
+package com.udriving.drivingapi.controller.response;
+
+/**
+ * 修改活动接口返回值
+ */
+public class MoidfyActivityResponse {
+    /**
+     * 操作结果
+     */
+    private boolean operationResult = true;
+}
