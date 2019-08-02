3cc31968b81b62d9942bb9d0e8276d9e5b70e544
liuhao <liuhaoduoduo@163.com>
2019-08-02 23:14:26 +0800

    修改启动类启动netty

--- boot/src/main/java/com/udriving/boot/ServiceBoot.java
+++ boot/src/main/java/com/udriving/boot/ServiceBoot.java
@@ -1,8 +1,21 @@
 package com.udriving.boot;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import lombok.AllArgsConstructor;
+
 /**
  * 服务器启动类
  */
+@AllArgsConstructor
 public class ServiceBoot {
     /**
      * 端口
@@ -15,6 +28,40 @@ public class ServiceBoot {
      * @param args 参数数组
      */
     public static void main(String[] args) {
-        System.out.println("hello word");
+        if (args.length >= 1) {
+            System.out.println("启动端口号：" + args[0]);
+            new ServiceBoot(Integer.parseInt(args[0])).bootService();
+            System.out.println("启动完成");
+        }
+    }
+
+    private void bootService() {
+        //用于服务器端接受客户端的连接
+        EventLoopGroup bossGruop = new NioEventLoopGroup();
+        //用于处理请求
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGruop, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel arg0) {
+                            arg0.pipeline()
+                                    .addLast(new HttpResponseEncoder(),
+                                            new HttpRequestDecoder(),
+                                            new RequestDistribute());
+                        }
+                    })
+                    //指定此套接口排队的最大连接个数
+                    .option(ChannelOption.SO_BACKLOG, 1024);
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGruop.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
     }
 }
