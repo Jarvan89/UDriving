4454f1756f2ef470c3768c31be4b15901a6742d4
liuhao <liuhaoduoduo@163.com>
2019-08-02 21:46:54 +0800

    增加启动模块

---
+++ boot/src/main/java/com/udriving/boot/ServiceBoot.java
@@ -0,0 +1,20 @@
+package com.udriving.boot;
+
+/**
+ * 服务器启动类
+ */
+public class ServiceBoot {
+    /**
+     * 端口
+     */
+    private int port;
+
+    /**
+     * 主入口
+     *
+     * @param args 参数数组
+     */
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
